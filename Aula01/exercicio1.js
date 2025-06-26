f257ef9303a861e26afde533fc3a8671437999c3
MateusGarciaaa <mateusgarcia3081@gmail.com>
2025-06-26 00:19:37 -0300

    comeco de potencia

--- Aula01/exercicio1.js
+++ Aula01/exercicio1.js
@@ -1,35 +1,30 @@
-let valor1 = '';
-let operacao = '';
-let valor2 = '';
 let expressao = '';
 
-
-function mudarTexto() {
-  let nome = document.getElementById("nome").value;
-  document.getElementById("message").innerText = "a";
-
-}
 
 function loadNum(num) {
   expressao = expressao + num;
   document.getElementById("display").value = expressao;
-  
+
 }
 
 function loadOperation(op) {
-  if(op==='x') op = '*';
+  if (op === 'x') op = '*';
   expressao = expressao + op
   document.getElementById("display").value = expressao;
 }
 
 
-function loadResult(){
+function loadResult() {
   resultado = eval(expressao);
-  document.getElementById("display").value = resultado;
-
+  expressao = resultado
+  document.getElementById("display").value = expressao;
 }
 
-function loadClear(){
+function loadClear() {
   expressao = '';
   document.getElementById("display").value = expressao;
+}
+
+function loadPotencia(){
+  
 }
