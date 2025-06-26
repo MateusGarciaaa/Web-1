0de6ad1cf43b2e4902b66e0919575d4c4daa636d
MateusGarciaaa <mateusgarcia3081@gmail.com>
2025-06-25 23:35:42 -0300

    algumas alteracoes, nada correto ainda

--- Aula01/exercicio1.js
+++ Aula01/exercicio1.js
@@ -1,12 +1,20 @@
+let valor1 = '';
+let operacao = '';
+let valor2 = '';
+
+
 function mudarTexto() {
-    let nome = document.getElementById("nome").value;
-  document.getElementById("message").innerText = nome;
+  let nome = document.getElementById("nome").value;
+  document.getElementById("message").innerText = "a";
+
+}
+
+function loadNum(num) {
+  let num = num;
+  document.getElementById("display").value;  
   
 }
 
-function mudarEstilo(){
-    const p = document.getElementById("message");
+function loadOperation() {
 
-    p.style.color = "blue";
-    p.style.fontSize = "32px";
 }
