4fb4121d8813a596f6d89dc46e945e2ba86b737e
MateusGarciaaa <mateusgarcia3081@gmail.com>
2025-06-25 22:16:16 -0300

    input adicionado

---
+++ Aula01/exercicio1.js
@@ -0,0 +1,12 @@
+function mudarTexto() {
+    let nome = document.getElementById("nome").value;
+  document.getElementById("message").innerText = nome;
+  
+}
+
+function mudarEstilo(){
+    const p = document.getElementById("message");
+
+    p.style.color = "blue";
+    p.style.fontSize = "32px";
+}
